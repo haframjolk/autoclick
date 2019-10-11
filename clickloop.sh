f3d9a6d3e19cc0b768e8f675f9a14ca25ba0648d
Terrance Wood <tj@ixqus.com>
2019-10-11 02:34:20 -0700

    Changed clickloop script to accept CLICK_COORDINATES as its first arg, SLEEP_INTERVAL as its second; will default to 0,0/.25 if omitted. (Preserves original intended script functionality, but fractional sleep actually works on macOS with the perl script.)

--- clickloop.sh
+++ clickloop.sh
@@ -1,7 +1,10 @@
 #!/bin/zsh
 
+CLICK_COORDINATES=${1:-0,0}
+SLEEP_INTERVAL=${2:-.25}
+
 while :
 do
-    /usr/local/bin/cliclick c:.
-    sleep 0.1
+   /usr/local/bin/cliclick c:$CLICK_COORDINATES
+   perl -e "select(undef,undef,undef,$SLEEP_INTERVAL);"
 done
